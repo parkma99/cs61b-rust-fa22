173d334d622914195b281de7e99014270c99feab
parkma99 <park-ma@hotmail.com>
2023-01-06 22:02:19 +0800

    lab4

--- lab04/src/flik.rs
+++ lab04/src/flik.rs
@@ -4,8 +4,5 @@
 /// An Integer tester created by Flik Enterprises.
 /// Uses cutting-edge match technology.
 pub fn is_same_number(a: u64, b: u64) -> bool {
-    match b {
-        a => true,
-        _ => false,
-    }
+    a == b
 }
