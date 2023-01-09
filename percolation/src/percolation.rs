fb336ac3e9897e1513d9e976d678e885efbb021b
parkma99 <park-ma@hotmail.com>
2023-01-09 22:07:06 +0800

    finish percolation

--- percolation/src/percolation.rs
+++ percolation/src/percolation.rs
@@ -1,7 +1,11 @@
 use crate::wqu::WeightedQuickUnion;
 
 pub struct Percolation {
-    // add any struct fields you need
+    grid: Vec<bool>,
+    wqu: WeightedQuickUnion,
+    width: usize,
+    height: usize,
+    nums_of_open: usize,
 }
 
 pub trait Percolatable {
@@ -15,36 +19,95 @@ pub trait Percolatable {
     fn percolates(&self) -> bool;
 }
 
+impl Percolation {
+    fn index(&self, row: usize, col: usize) -> usize {
+        row * self.width + col + 1
+    }
+    fn is_valid(&self, row: usize, col: usize) -> bool {
+        row < self.height && col < self.width
+    }
+}
+
 impl Percolatable for Percolation {
     fn new(width: usize, height: usize) -> Self {
-        unimplemented!() // TODO: fill in this method
+        let size = width * height + 2;
+        let mut grid = Vec::new();
+        grid.resize(size, false);
+        grid[0] = true;
+        grid[size - 1] = true;
+        let wqu = WeightedQuickUnion::new(size);
+        Self {
+            grid,
+            wqu,
+            width,
+            height,
+            nums_of_open: 0,
+        }
     }
 
     fn width(&self) -> usize {
-        unimplemented!() // TODO: fill in this method
+        self.width
     }
 
     fn height(&self) -> usize {
-        unimplemented!() // TODO: fill in this method
+        self.height
     }
 
     fn open(&mut self, row: usize, col: usize) {
-        unimplemented!() // TODO: fill in this method
+        if self.is_valid(row, col) && !self.is_open(row, col) {
+            let index = self.index(row, col);
+            self.grid[index] = true;
+            self.nums_of_open += 1;
+            let mut neighbors = Vec::new();
+            if row > 0 && self.is_open(row - 1, col) {
+                neighbors.push(self.index(row - 1, col));
+            }
+            if row < self.height - 1 && self.is_open(row + 1, col) {
+                neighbors.push(self.index(row + 1, col));
+            }
+            if col > 0 && self.is_open(row, col - 1) {
+                neighbors.push(self.index(row, col - 1));
+            }
+            if col < self.width - 1 && self.is_open(row, col + 1) {
+                neighbors.push(self.index(row, col + 1));
+            }
+            for neighbor in neighbors {
+                self.wqu.union(index, neighbor);
+            }
+            if row == 0 {
+                self.wqu.union(0, index);
+            }
+            if row == self.height - 1 {
+                self.wqu.union(self.grid.len() - 1, index);
+            }
+        }
     }
 
     fn is_open(&self, row: usize, col: usize) -> bool {
-        unimplemented!() // TODO: fill in this method
+        match self.is_valid(row, col) {
+            true => {
+                let index = self.index(row, col);
+                self.grid[index]
+            }
+            false => false,
+        }
     }
 
     fn is_full(&self, row: usize, col: usize) -> bool {
-        unimplemented!() // TODO: fill in this method
+        match self.is_valid(row, col) {
+            true => {
+                let index = self.index(row, col);
+                self.wqu.connected(0, index)
+            }
+            false => false,
+        }
     }
 
     fn number_of_open_sites(&self) -> usize {
-        unimplemented!() // TODO: fill in this method
+        self.nums_of_open
     }
 
     fn percolates(&self) -> bool {
-        unimplemented!() // TODO: fill in this method
+        self.wqu.connected(0, self.grid.len() - 1)
     }
 }
--- percolation/src/percolationstats.rs
+++ percolation/src/percolationstats.rs
@@ -27,5 +27,37 @@ pub fn calculate_stats<P: Percolatable>(
     trials: usize,
     random: &mut Random,
 ) -> PercolationStats {
-    unimplemented!() // TODO: fill in this method
+    let mut counts = Vec::new();
+    counts.resize(width * height, 0);
+    for _ in 0..trials {
+        let mut percolation = P::new(width, height);
+        while !percolation.percolates() {
+            let row = random.next_below(height);
+            let col = random.next_below(width);
+            percolation.open(row, col);
+        }
+        counts[percolation.number_of_open_sites()] += 1;
+    }
+    let mut mean = 0.0;
+    let mut stddev = 0.0;
+    for (i, count) in counts.iter().enumerate() {
+        let ratio = i as f64 / (width * height) as f64;
+        mean += (ratio * *count as f64) / trials as f64;
+    }
+    for (i, count) in counts.iter().enumerate() {
+        let ratio = i as f64 / (width * height) as f64;
+        stddev += (ratio - mean).powi(2) * *count as f64;
+    }
+    stddev /= trials as f64 - 1.0;
+    stddev = stddev.sqrt();
+
+    let confidence_low = mean - 1.96 * stddev / (trials as f64).sqrt();
+    let confidence_high = mean + 1.96 * stddev / (trials as f64).sqrt();
+    PercolationStats {
+        counts,
+        mean,
+        stddev,
+        confidence_low,
+        confidence_high,
+    }
 }
